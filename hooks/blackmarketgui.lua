3ad180938e19b7a85edc65275ee9a43ddcd1e7dc
offyerrocker <offyerrocker88@gmail.com>
2026-04-25 01:12:15 -0700

    default gadget, localization
    reenabled "Change Default Sight Reticule and Gadget Color" qol feature
    fixed issue with aforementioned qol feature where texture switch would not be reflected in ui
    fixed npc flashlights not having a menu generated for them
    rebrand "LasersPlus" -> "Lasers+" because I forgot menu text is all capitalized and it doesn't look good as "LASERSPLUS"

--- hooks/blackmarketgui.lua
+++ hooks/blackmarketgui.lua
@@ -4,19 +4,14 @@ Hooks:PostHook(BlackMarketGui,"_buy_mod_callback","LasersPlus_BlackMarketGui_pos
 	local slot = data.slot
 	local category = data.category
 	local factory = tweak_data.weapon.factory.parts[id]
-
 	if factory then
+		local qol_enabled = LasersPlus:IsQOLDefaultGadgetEnabled()
 		if factory.texture_switch then
-			local default
-			
-			local qol_enabled = LasersPlus:IsQOLDefaultGadgetEnabled()
-			
 			if qol_enabled then
-				default = tostring(LasersPlus:GetSightColorIndex()) .. " " .. tostring(LasersPlus:GetSightTextureIndex())
-			else
-				default = tweak_data.gui.part_texture_switches[data.name] or tweak_data.gui.default_part_texture_switch
+				local data_string = tostring(LasersPlus:GetSightColorIndex()) .. " " .. tostring(LasersPlus:GetSightTextureIndex())
+				managers.blackmarket:set_part_texture_switch(category, slot, id, data_string)
+				self:reload()
 			end
-			managers.blackmarket:set_part_texture_switch(category, slot, id, default)
 		else
 			local override_laser = qol_enabled
 			local override_flashlight = qol_enabled
@@ -26,17 +21,14 @@ Hooks:PostHook(BlackMarketGui,"_buy_mod_callback","LasersPlus_BlackMarketGui_pos
 					local laser_color = part_colors.laser
 					if override_laser and laser_color then 
 						part_colors.laser = Color(LasersPlus.settings.user_laser_color)
---						LasersPlus:log("New override laser color: " .. tostring(ColorPicker.color_to_hex(part_colors.laser)),{color=part_colors.laser})
 					end
 					
 					local flashlight_color = part_colors.flashlight
 					if override_flashlight and flashlight_color then 
 						part_colors.flashlight = Color(LasersPlus.settings.user_flash_color)
---						LasersPlus:log("New override flashlight color: " .. tostring(ColorPicker.color_to_hex(part_colors.flashlight)),{color=part_colors.flashlight})
 					end
 					managers.blackmarket:set_part_custom_colors(data.category,data.slot,data.name,part_colors)
-				else
---					LasersPlus:log("BlackMarketGui:_buy_mod_callback(): No part by name " .. tostring(id))
+					self:reload()
 				end
 			end
 		end
